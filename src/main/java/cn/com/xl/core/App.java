cee28a8ab52237bc020270cf7373d7ba36bf99a2
xieliang85a <xieliang85a@163.com>
2015-06-24 14:04:18 +0800

    测试提交

--- src/main/java/cn/com/xl/core/App.java
+++ src/main/java/cn/com/xl/core/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "小2小样说：" +  "Hello World!" );
+        System.out.println( "小22小样说：" +  "Hello World!" );
         
         System.out.println(BeanUtils.class.getName());
     }
